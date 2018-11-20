90c00848c766bf18f1a7ceeb6d77bd80e1d93947
Jim.Li <Jim.Li@jodoinc.com>
2018-11-20 20:02:32 +0800

    update

---
+++ HandsOnProject/Assets/Coordinates/CoordinatesCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoordinatesCamera : MonoBehaviour 
+{
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+		var camera = GetComponent<Camera>();
+		print("WorldToScreenPoint:" + camera.WorldToScreenPoint(target.transform.position));
+		print("WorldToViewportPoint:"+camera.WorldToViewportPoint(target.transform.position));
+		print("worldToCameraMatrix:" + camera.worldToCameraMatrix.MultiplyPoint(target.transform.position));
+		print("worldToLocalMatrix:" + transform.worldToLocalMatrix.MultiplyPoint(target.transform.position));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
